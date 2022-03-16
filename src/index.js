57ef08e4d55f9b325bf34ed0a9896e8e25f87b1b
George Nyagura <57874698+NumNine9@users.noreply.github.com>
2022-03-16 23:11:23 +0100

    Revert "Made a basic structure of the home page"

--- src/index.js
+++ src/index.js
@@ -21,24 +21,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
 const db = getFirestore();
-document.addEventListener('DOMContentLoaded', function() {
-  const loadEl = document.querySelector('#load');
-  try {
-    let app = firebase.app();
-    let features = [
-      'auth', 
-      'database', 
-      'firestore',
-      'functions',
-      'messaging', 
-      'storage', 
-      'analytics', 
-      'remoteConfig',
-      'performance',
-    ].filter(feature => typeof app[feature] === 'function');
-    loadEl.textContent = `Firebase SDK loaded with ${features.join(', ')}`;
-  } catch (e) {
-    console.error(e);
-    loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
-  }
-});
